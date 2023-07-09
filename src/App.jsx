0a26f3b525b446ca7cd95e0e069d2a09a5fc48d8
Erick <123erickrh@gmail.com>
2023-07-09 17:57:57 -0500

    Install vite-plugin-svgr. Add logo as component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import './App.css'
 import { Searcher } from './components/Searcher';
 import { PokemonList } from './components/PokemonList';
 import { Col } from 'antd';
+import { ReactComponent as Logo } from './statics/logo.svg';
 
 function App() {
 
   return (
     <div className="app">
+      <Col span={4} offset={10}>
+        <Logo className='Logo' />
+      </Col>
+
       <Col span={8} offset={8}>
         <Searcher />
       </Col>
