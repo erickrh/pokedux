95d14c699507623e6373df4ca16a9287c164c62c
Erick <123erickrh@gmail.com>
2023-07-16 15:11:31 -0500

    Remove useless code and files

--- src/usePokeAPI/index.js
+++ src/usePokeAPI/index.js
@@ -1,26 +1,3 @@
-// VERSION 1.0
-// import axios from 'axios';
-
-// function usePokeAPI() {
-//   const api = axios.create({
-//     baseURL: 'https://pokeapi.co/api/v2/',
-//   });
-
-//   const getPokemon = async () => {
-//     const { data } = await api('pokemon?limit=151');
-//     return data.results;
-//   };
-
-//   const getPokemonDetails = async pokemon => {
-//     const { data } = await api(pokemon.url);
-//     return data;
-//   }
-
-//   return { getPokemon, getPokemonDetails };
-// }
-
-// export { usePokeAPI };
-
 import axios from 'axios';
 
 const api = axios.create({
