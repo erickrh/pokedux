433acdeaa67f0d04491f1776adec82d586fc9fd2
Erick <123erickrh@gmail.com>
2023-07-22 13:13:07 -0500

    Add logo container

--- src/App.jsx
+++ src/App.jsx
@@ -30,10 +30,12 @@ function App() {
   return (
     <div className='App'>
       <Col span={6} offset={9}>
-        <Logo
-          className='Logo'
-          onClick={() => goHome(dispatch)}
-        />
+        <div className="LogoContainer">
+          <Logo
+            className='Logo'
+            onClick={() => goHome(dispatch)}
+          />
+        </div>
       </Col>
       
       <div className="searcherContainer">
